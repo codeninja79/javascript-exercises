4a17e73f8447ea712389d2baa28649561f1c1039
codeninja79 <oroborosrandy@gmail.com>
2023-09-05 02:42:42 +0530

    completed 1st 2 exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(word,times) {
+    let ans='';
+    if(times<0)
+        return 'ERROR'
+    for(let i=0;i<times;i++){
+            ans+=word
+         
+    }
+    return ans;
 };
 
 // Do not edit below this line
