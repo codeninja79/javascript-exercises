66f961cf3d27eaac91fa15ec710e40b4adea618e
codeninja79 <oroborosrandy@gmail.com>
2023-09-17 22:20:29 +0530

    Completed ex's 05 to 07 without any hints, feeling pretty good hehe

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function(n1,n2) {
+    if(n1<0 || n2<0 || Number.isInteger(n1)==false || Number.isInteger(n2)==false)
+      return "ERROR";
+    let sum=0
+    if(n1>n2){
+    for(let i=n1;i>=n2;i--){
+      sum+=i;
+    }
+    return sum;
+  }
+  if(n1<n2){
+    for(let i=n1;i<=n2;i++){
+      sum+=i;
+    }
+    return sum;
+  }
+    
 };
 
 // Do not edit below this line
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,14 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let ans=(temp-32)*5/9
+  let ansRounded = Math.round(ans*10)/10
+  return ansRounded;
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let ans=(temp*(9/5) + 32)
+  let ansRounded = Math.round(ans*10)/10
+  return ansRounded;
 };
 
 // Do not edit below this line
